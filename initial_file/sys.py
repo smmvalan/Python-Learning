e62ac2cce33f435783ddc66ec0b3f6d0e44aa11f
smmvalan <smmvalan@yahoo.com>
2020-07-01 09:04:52 +0100

    initial

---
+++ initial_file/sys.py
@@ -0,0 +1,28 @@
+import datetime
+
+
+
+ti = datetime.datetime.now()
+t2=datetime.time(8,54,20)
+
+print("time{}\n hour {}" .format(ti,t2))
+
+'''
+d=datetime.date(2020,6,29)
+print(d)
+tday=datetime.date.today()
+
+t=datetime.time(1,2,3,0)
+print(t.hour)
+print(t.minute)
+print(t.second)
+print(t.microsecond)
+dt=datetime.datetime.combine(d,t)
+print(dt)
+
+print(datetime.datetime.now())
+
+td=datetime.timedelta(days=3,seconds=2,microseconds=3,minutes=3,hours=2,weeks=5)
+print(td)
+
+'''
---
+++ initial_file/twinkle.py
@@ -0,0 +1,3 @@
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are! \n\t\t Up above the world so high,\
+    \nLike a diamond in the sky. \nTwinkle,\twinkle, little star,\
+    \n\t How I wonder what you are" )
