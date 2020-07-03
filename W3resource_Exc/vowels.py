bf0dce57d80cdd4ca91c03ebeda792832234d5d9
smmvalan <smmvalan@yahoo.com>
2020-07-03 10:48:41 +0100

    init

---
+++ W3resource_Exc/vowels.py
@@ -0,0 +1,31 @@
+'''
+vowel=0
+consonents=0
+for letter in "onion":
+    if letter.lower() in"aeiou":
+        vowel=vowel+1
+    else:
+        consonents=consonents+1
+        print("Vowels:{}".format(vowel))
+        print("Consonents:{}".format(consonents))
+ 
+'''
+def vowelConsonentDisplay():
+    vowels=0
+    consonents=0
+   
+
+    for letter in sta:
+        if letter.lower() in "aeiou" :
+            vowels=vowels+1
+        else:
+            consonents=consonents+1
+
+def main():
+    sta=input("Enter the statement") 
+    print(vowels)
+    print(consonents)
+    vowelConsonentDisplay()
+
+main()
+
